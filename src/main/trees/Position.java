d86e976d007188b2207adcc6fd267559c16b4337
takunnithan <takunnithan@gmail.com>
2016-11-17 02:20:26 +0530

    Minor changes

--- src/main/trees/Position.java
+++ src/main/trees/Position.java
@@ -1,8 +1,14 @@
 package main.trees;
 
+import java.util.Iterator;
+
 /**
  * Created by takunnithan on 12-11-2016.
  */
-public class Position<E> extends Object{
+public class Position<E> implements Iterable<E>{
 
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
 }
